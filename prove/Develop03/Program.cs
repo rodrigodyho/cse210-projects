2b909244eabdb4d53054571b5de55b9b143dd39f
Rodrigo Lima <56695576+rodrigodyho@users.noreply.github.com>
2023-02-10 22:02:39 -0300

    W06 Prove: Developer (Encapsulation)

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,29 @@
+
 using System;
 
+//As a stretch challenge, I tried to randomly select from only those words that are not already hidden.
 class Program
 {
   static void Main(string[] args)
   {
+    Reference scriptureReference = new Reference("Proverbs", "3", "5-6");
+    Scripture scripture = new Scripture(scriptureReference, "Trust in the lord with all thine heart and understanding; in all thy ways acknowledge him, and he shall direct thy paths.");
+    Word scriptureMemorizer = new Word(scripture);
 
+    string userInput = "";
+
+    while (userInput != "quit" && scriptureMemorizer.hasWordsLeft() == true)
+    {
+      Console.Clear();
+      Console.WriteLine(string.Format("{0} {1}", scriptureReference.toString(), scriptureMemorizer.toString()));
+      Console.WriteLine();
+      userInput = Console.ReadLine();
+      scriptureMemorizer.removeWordsFromText();
+    }
+    //one more time outside of while loop since program ends with one word left
+    Console.Clear();
+    Console.WriteLine(string.Format("{0} {1}", scriptureReference.toString(), scriptureMemorizer.toString()));
+    Console.WriteLine();
+    Console.WriteLine("Good job!");
   }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Scripture
+{
+  private string _scriptureText;
+  private Reference _scriptureReference;
+
+  public Scripture(Reference scriptureReference, string scriptureText)
+  {
+    _scriptureReference = scriptureReference;
+    _scriptureText = scriptureText;
+  }
+
+  public string toString()
+  {
+    return string.Format("{0}", _scriptureText);
+
+
+  }
+}
